0760513d92a82165374999a0e2be78962d927437
nagarajudbng <nagaraj.dbng@gmail.com>
2023-11-22 12:20:33 +0530

    Flutter integration
    Flutter integrated into native app

---
+++ CourtCount_2/app/src/main/java/com/example/courtcount_2/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.courtcount_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import com.example.courtcount_2.ui.theme.CourtCount_2Theme
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CourtCount_2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    Greeting("Android")
+                    SimpleButton()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun SimpleButton(){
+    var context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Button(
+            onClick = {
+//                context.startActivity(FlutterActivity.createDefaultIntent(context))
+//                context.startActivity(FlutterActivity.withNewEngine().initialRoute("/my_route").build(context))
+                context.startActivity(FlutterActivity.withCachedEngine("my_engine_id").build(context))
+            }) {
+            Text(text = "Simple Button")
+        }
+    }
+
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CourtCount_2Theme {
+        Greeting("Android")
+    }
+}
---
+++ CourtCount_2/app/src/main/java/com/example/courtcount_2/application/MyApplication.kt
@@ -0,0 +1,21 @@
+package com.example.courtcount_2.application
+
+import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class MyApplication : Application() {
+    lateinit var flutterEngine: FlutterEngine
+
+    override fun onCreate() {
+        super.onCreate()
+
+        flutterEngine = FlutterEngine(this)
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+        FlutterEngineCache.getInstance().put("my_engine_id",flutterEngine)
+    }
+
+}
